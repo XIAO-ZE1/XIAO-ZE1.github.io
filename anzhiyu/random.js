7eb90d572852896c34f7601a4884d2a9274e9a66
xiaoze <3337484367@qq.com>
2024-11-02 06:02:36 +0800

    Site updated: 2024-11-02 06:02:35

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/undefined.html","posts/ae28.html","posts/ddae.html","posts/2013454d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
