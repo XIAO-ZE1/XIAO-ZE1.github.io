31b06a31ba319eb321338560aeb3495c5bc7ecb6
xiaoze <3337484367@qq.com>
2025-01-09 01:14:59 +0800

    Site updated: 2025-01-09 01:14:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/08/hello-world/"];function toRandomPost(){
+var posts=["posts/16107.html","posts/ae28.html","posts/ddae.html","posts/3458e8f8.html","posts/371d898c.html","posts/b2daadad.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
